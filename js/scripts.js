1a9f41e643d364fe167ce21f2eaec4fffc9f4f9e
vyacable <vyacable@gmail.com>
2020-10-31 16:58:53 -0400

    modal1

--- js/scripts.js
+++ js/scripts.js
@@ -119,29 +119,47 @@
 })(jQuery); // End of use strict
 
 /** Checkout */
-    
+/****Modal1****/ 
+(function() {
+  var stripe = Stripe('pk_live_51HbWAyDU8E6zoNrNPP3OjJJ3j6ccTEwVR3HLchsWyMJbtP7s6DYzqDdeb9Hdy8pkWG8Rv8FeK3kuZuJD30iD8ROh00ZC0e00oL');
+
+  var checkoutButton = document.getElementById('checkout-button-price_1HiLSxDU8E6zoNrNkdmOMmod');
+  checkoutButton.addEventListener('click', function () {
+
+    stripe.redirectToCheckout({
+      lineItems: [{price: 'price_1HiLSxDU8E6zoNrNkdmOMmod', quantity: 1}],
+      mode: 'payment',
+
+      successUrl: window.location.protocol + '//optiwifi.netlify.app/Succes',
+      cancelUrl: window.location.protocol + '//optiwifi.netlify.app/Error',
+    })
+    .then(function (result) {
+      if (result.error) {
+
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
+})();
+
+
+/****Modal2****/   
+
+
+/****Modal3****/    
 (function() {
     var stripe = Stripe('pk_live_51HbWAyDU8E6zoNrNPP3OjJJ3j6ccTEwVR3HLchsWyMJbtP7s6DYzqDdeb9Hdy8pkWG8Rv8FeK3kuZuJD30iD8ROh00ZC0e00oL');
-  
     var checkoutButton = document.getElementById('checkout-button-price_1HiLVmDU8E6zoNrN2pP4PSHg');
     checkoutButton.addEventListener('click', function () {
-      // When the customer clicks on the button, redirect
-      // them to Checkout.
       stripe.redirectToCheckout({
         lineItems: [{price: 'price_1HiLVmDU8E6zoNrN2pP4PSHg', quantity: 1}],
         mode: 'payment',
-        // Do not rely on the redirect to the successUrl for fulfilling
-        // purchases, customers may not always reach the success_url after
-        // a successful payment.
-        // Instead use one of the strategies described in
-        // https://stripe.com/docs/payments/checkout/fulfill-orders
         successUrl: window.location.protocol + '//optiwifi.netlify.app/Succes',
         cancelUrl: window.location.protocol + '//optiwifi.netlify.app/Error',
       })
       .then(function (result) {
         if (result.error) {
-          // If `redirectToCheckout` fails due to a browser or network
-          // error, display the localized error message to your customer.
           var displayError = document.getElementById('error-message');
           displayError.textContent = result.error.message;
         }
